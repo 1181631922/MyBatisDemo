018fcc866dd5e7804dc9a1c73e560f9e5b8ab1f9
fanyafeng <fanyafeng@365rili.com>
2016-10-12 11:16:20 +0800

    mybatis完成增删改查操作

--- src/main/java/com/fanyafeng/dao/IUserDao.java
+++ src/main/java/com/fanyafeng/dao/IUserDao.java
@@ -2,11 +2,22 @@ package com.fanyafeng.dao;
 
 import com.fanyafeng.model.User;
 
+import java.util.List;
+
 /**
  * Author： fanyafeng
  * Data： 16/10/11 14:55
  * Email: fanyafeng@live.cn
  */
 public interface IUserDao {
+    public List<User> queryUserByName(String name);
+
     public User selectUserById(int id);
+
+    public void add();
+
+    public void del(int id);
+
+    public void alter(int id);
+
 }
--- src/main/java/com/fanyafeng/test/UserTest.java
+++ src/main/java/com/fanyafeng/test/UserTest.java
@@ -8,6 +8,8 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Author： fanyafeng
@@ -30,11 +32,47 @@ public class UserTest {
     public static void main(String[] args) {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         try {
-            User user = (User) sqlSession.selectOne("com.fanyafeng.dao.IUserDao.selectUserById", 2);
-            System.out.println(user.getAddress() + user.getSex() + user.getBirthday());
-            System.out.println(user.getId() + user.getUsername());
+//            单个数据查询
+//            User user = (User) sqlSession.selectOne("com.fanyafeng.dao.IUserDao.selectUserById", 2);
+//            System.out.println(user.getAddress() + user.getSex() + user.getBirthday());
+//            System.out.println(user.getId() + user.getUsername());
+//
+//
+//            列表查询
+//            List<User> userList = sqlSession.selectList("com.fanyafeng.dao.IUserDao.queryUserByName", "%李宁%");
+//            for (int i = 0; i < userList.size(); i++) {
+//                System.out.println(userList.get(i).toString());
+//            }
+
+//            插入操作
+            User user = new User();
+//            user.setId(100);
+            user.setBirthday(new Date());
+            user.setUsername("李宁");
+            user.setSex("女");
+            user.setAddress("家里蹲");
+            int isAdd = sqlSession.insert("com.fanyafeng.dao.IUserDao.add", user);
+//            进行事物操作后一定要commit,不然不会生效
+            sqlSession.commit();//不要忘记
+
+
+//            根据id进行删除,感觉在删除之前应该先进行查询,存在的话删除,不存在的话给相应的提示
+//            这里是初学测试没有做那么麻烦
+//            sqlSession.delete("com.fanyafeng.dao.IUserDao.del", 100);
+//            sqlSession.commit();
+
+
+//            根据id更新数据库
+//            User user = (User) sqlSession.selectOne("com.fanyafeng.dao.IUserDao.selectUserById", 1);
+//            user.setBirthday(new Date());
+//            user.setAddress("更新后的信息");
+//            sqlSession.update("com.fanyafeng.dao.IUserDao.alter", user);
+//            sqlSession.commit();
+
         } finally {
             sqlSession.close();
         }
     }
+
+
 }
